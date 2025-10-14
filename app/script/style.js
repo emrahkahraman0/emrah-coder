a53b16278bf4af7546dd1547115ab0b02aba1b31
Emrah Kahraman <oempire34@gmail.com>
2025-10-14 16:50:34 +0300

    update-7

--- app/script/style.js
+++ app/script/style.js
@@ -120,4 +120,20 @@ $(document).ready(function () {
         }
       );
   });
+
+  //Faqs
+  var acc = document.getElementsByClassName("faqs_item_btn");
+  var i;
+
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      }
+    });
+  }
 });
